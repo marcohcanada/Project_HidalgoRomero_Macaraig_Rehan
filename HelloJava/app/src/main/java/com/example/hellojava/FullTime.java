ff6c0879c541afe8c1638594d13208e5413cc15e
marcohidalgo <marcohcanada@gmail.com>
2019-05-20 21:27:19 -0400

    Person and Employee classes added. Performed Inheritance. FullTime and PartTime classes added.

---
+++ HelloJava/app/src/main/java/com/example/hellojava/FullTime.java
@@ -0,0 +1,54 @@
+package com.example.hellojava;
+
+/**
+ * Created by 991470628 : MARCO HIDALGO ROMERO
+ * on 2019-05-16
+ */
+public class FullTime extends Employee {
+    double basicPay;
+    double hoursWorked;
+    double finalPay;
+
+    FullTime(){
+        this.basicPay = 50;
+        this.hoursWorked = 40;
+        this.finalPay = basicPay * hoursWorked;
+    }
+
+    FullTime(double basicPay, double hoursWorked, double finalPay, double pay, String fnm, String lnm, int age){
+        super(pay, fnm, lnm, age);
+        this.basicPay = basicPay;
+        this.hoursWorked = hoursWorked;
+        this.finalPay = finalPay;
+    }
+
+    public void setBasicPay(double basicPay) {
+        this.basicPay = basicPay;
+    }
+
+    public double getBasicPay() {
+        return basicPay;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setFinalPay(double finalPay) {
+        this.finalPay = finalPay;
+    }
+
+    public double getFinalPay() {
+        return finalPay;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nBasic Pay : " + this.basicPay + "\nHours Worked : "
+                + this.hoursWorked + "\nFinal Pay : " + this.finalPay;
+    }
+}
--- HelloJava/app/src/main/java/com/example/hellojava/PayCalc.java
+++ HelloJava/app/src/main/java/com/example/hellojava/PayCalc.java
@@ -21,5 +21,34 @@ public class PayCalc {
         prof.setAge(50);
         prof.setPay(20.50);
         System.out.println(prof.toString());
+
+        FullTime fullTimeProf = new FullTime();
+        fullTimeProf.setFirstName("Severus");
+        fullTimeProf.setLastName("Snape");
+        fullTimeProf.setAge(38);
+        fullTimeProf.getBasicPay();
+        fullTimeProf.getHoursWorked();
+        fullTimeProf.getFinalPay();
+        System.out.println(fullTimeProf.toString());
+
+        PartTime partTimeProf = new PartTime();
+        partTimeProf.setFirstName("Remus");
+        partTimeProf.setLastName("Lupin");
+        partTimeProf.setAge(38);
+        partTimeProf.getBasicPay();
+        partTimeProf.getHoursWorked();
+        partTimeProf.getFinalPay();
+        System.out.println(partTimeProf.toString());
     }
 }
+
+/*Experiment:
+
+Add type of employee (full-time or part-time)
+Add data member to indicate the basic pay of employee
+(basic pay for part-time is $20, full-time is $50)
+Add a variable to represent the number of hours the employee has worked.
+
+add a method to calculate the final Pay for the employee considering
+the number of hours they have worked and their status.
+ */
